de5ef908787e190d1557cfd33bc7eb2a4f2d1e13
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-22 13:47:07 -0400

    added mongodb connection to app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Establish db connection
+const mongoose = require("mongoose");
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
