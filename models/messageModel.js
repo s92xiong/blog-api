e6570b81723b4a8f550c7c228fbc703ad350b27d
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-22 23:25:19 -0400

    Added date virtual for messageModel.js

--- models/messageModel.js
+++ models/messageModel.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-  title: { type: String, required: true, minLength: 1, maxLength: 100 },
   text: { type: String, required: true, maxLength: 10000 },
   timestamp: { type: Date, default: Date.now, required: true }
+});
+
+MessageSchema.virtual("date").get(function() {
+  return DateTime.fromJSDate(this.timestamp).toFormat("yyyy-MM-dd, HH:mm");
 });
 
 module.exports = mongoose.model("message", MessageSchema);
