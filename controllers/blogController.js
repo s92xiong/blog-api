ffc4604b4ae05d6fe18f0e115d98ec84866fcb74
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-24 16:05:29 -0400

    Added query to retrieve comments for a specific blog postt

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,10 +1,19 @@
 const Blog = require("../models/blogModel");
+const Comment = require("../models/commentModel");
 
 //  Show a specific blog post
-exports.blog_post_GET = async (req, res) => {
-  try {
-    const blog = await Blog.findById(req.params.postId);
-    res.json(blog);
+exports.blog_post_GET = async (req, res, next) => {
+  try {   
+    // Query for blog and comments (find all comments w/ blog_post ID that matches the current params ID)
+    const [blog, comments] = await Promise.all([
+      Blog.findById(req.params.postId),
+      Comment.find({ "blog_post": req.params.postId })
+    ]);
+
+    // Update the blog's comments with the queried comments & return blog to client
+    blog.comments = comments;
+    return res.json(blog);
+
   } catch (err) {
     console.error(err);
     return res.status(404).send("Error: cannot access blog post");
