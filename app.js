922227ed71214612205830dd13b3c4f582922471
Sheuh Xiong <sheuhxiong@gmail.com>
2021-08-31 13:49:12 -0400

    Enabled all CORS requests in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require("cors");
 require("dotenv").config();
 
 // Establish db connection
@@ -17,6 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 // Render the index.html file in public folder?
 // app.use(express.static(path.join(__dirname, 'public')));
