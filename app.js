4f72479d7dc81824a2cf47a949f1af3587d09152
Sheuh Xiong <sheuhxiong@gmail.com>
2022-12-03 15:42:50 -0500

    update app.js

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ const logger = require('morgan');
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const MONGODB_URI = `mongodb+srv://s92xiong:s92xiong-blog-api@cluster0.xfmld.mongodb.net/blog-api-db?retryWrites=true&w=majority`;
-
 // Establish db connection
-mongoose.connect(`${MONGODB_URI}`, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
