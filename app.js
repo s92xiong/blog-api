3ba0785fa45270c64b2410df454f7c450c7f4abd
Sheuh Xiong <sheuhxiong@gmail.com>
2022-12-03 14:06:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const MONGODB_URI = `mongodb+srv://s92xiong:${process.env.MONGODB_URI_pw}@cluster0.xfmld.mongodb.net/${process.env.MONGODB_URI_db}?retryWrites=true&w=majority`
+const MONGODB_URI = `mongodb+srv://s92xiong:s92xiong-blog-api@cluster0.xfmld.mongodb.net/blog-api-db?retryWrites=true&w=majority`;
 
 // Establish db connection
 mongoose.connect(`${MONGODB_URI}`, {
@@ -24,9 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-
-// Render the index.html file in public folder?
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
